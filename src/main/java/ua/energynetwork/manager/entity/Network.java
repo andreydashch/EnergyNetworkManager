aac247a499db55b160ccbc415449f4f3f8f75565
Andrey Dashchyk <andreydashch@gmail.com>
2020-01-29 01:04:37 +0200

    Form entity hierarchy

---
+++ src/main/java/ua/energynetwork/manager/entity/Network.java
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Andrey Dashchyk
+ *
+ * Permission is hereby granted, free of charge,to any
+ * person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission
+ * notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY
+ * OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ua.energynetwork.manager.entity;
+
+import java.util.HashSet;
+
+/**
+ * Date: 29.01.2020
+ * User: Andrey Dashchyk
+ */
+public interface Network<E> {
+    void create(E root);
+    void delNetwork();
+    void delChild(Long parentId, E child);
+    boolean addChild(Long parentId, E child);
+
+    E getRoot();
+    E getNode(Long Id);
+    HashSet<E> getChildren(Long Id);
+}
